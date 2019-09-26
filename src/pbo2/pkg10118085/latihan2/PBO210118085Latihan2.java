81568c3b7b2240aeedab45010f1923aaa6c2b66a
Risky Irfansyah <55643683+RiskyIrfansyah@users.noreply.github.com>
2019-09-26 08:01:23 +0700

    Add files via upload

---
+++ src/pbo2/pkg10118085/latihan2/PBO210118085Latihan2.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118085.latihan2;
+
+/**
+ *@author 
+ * NAMA                 : RISKY IRFRANSYAH
+ * KELAS                : IF-2
+ * NIM                  : 10118085
+ * DESKRIPSI PROGRAM    : Deklarasi & pengaksesan variabel
+ * @author RISKY
+ */
+public class PBO210118085Latihan2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Deklarasi Variabel
+        int nilaiInt;
+        final double PHI = 3.14; //konstanta uppercase
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        
+        //Memeberi nilai ke variabel
+        nilaiInt = 78;
+        nilaiLogika = false;
+        nilaiKarakter = 'D';
+        
+        //menampilkan hasil
+        System.out.println();
+        System.out.println("Isi variabel nilai = " + nilaiInt);
+        System.out.println("Isi variabel PHI = " + PHI);
+        System.out.println("Isi variabel logika = " + nilaiLogika);
+        System.out.println("Isi variabel Karakter = " + nilaiKarakter);
+    }
+    
+}
